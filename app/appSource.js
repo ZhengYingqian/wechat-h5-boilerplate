cbaf72d11e5ea65a3f278875cc4738b4fcf61cd5
ZhengYingqian <1585076433@qq.com>
2018-06-07 16:41:17 +0800

    fix middleware problems

--- app/appSource.js
+++ app/appSource.js
@@ -8,31 +8,30 @@ var express = require('express'),
     ejs = require('ejs'),
     app = express(),
     server = require('http').createServer(app);
-	// compression = require("compression");
-
-// app.use(compression({level:9}));//express compression to support gzip
+var createError = require('http-errors');
 
 app.set('port', process.env.PORT || 4331);
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html'); //替换文件扩展名ejs为html
-// app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'javascripts')));
-// app.use(express.static(path.join(__dirname, 'stylesheets')));
-// app.use(express.static(path.join(__dirname, 'images')));
-// app.use(express.static(path.join(__dirname, 'fonts')));
-// app.use(express.static(path.join(__dirname, 'audios')));
 app.use(express.static(path.join(__dirname, 'dist')));
-if (app.get('env') === 'development') {
-  app.use(express.errorHandler());
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 // angular启动页
 app.get('/', function (req, res) {
-  //res.sendfile('medWeb1.0/index.html');
     res.sendfile('dist/index.html');
 });
 
